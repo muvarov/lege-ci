4afb035cf67aaacb7b65103bd9f29aeeb0428d8f
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-07-12 18:26:41 +0300

    fix3
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,10 +7,11 @@ cd oe-linaro-ledge
 
 git config --global user.email "ci@linaro.org"
 git config --global user.name "LEGE CI"
+adduser --disabled-password --gecos "" build
+apt-get install sudo
 
 echo "N" | repo --color=never init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
 repo sync
 
-MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment
-bitbake lmp-gateway-image
-
+sudo -H -u build MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment
+sudo -H -u build bitbake lmp-gateway-image
