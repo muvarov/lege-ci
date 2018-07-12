cad892b8fcb26b5ba0631ab173f0986f39e04e36
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-07-12 18:51:54 +0300

    fix

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,25 @@
 #!/bin/bash -x
 
 set -e
-cd ~
+cd /opt
+chmod 777 /opt
+
 mkdir oe-linaro-ledge
 cd oe-linaro-ledge
 
 git config --global user.email "ci@linaro.org"
 git config --global user.name "LEGE CI"
 adduser --disabled-password --gecos "" build
-apt-get install sudo
+apt-get install -y --force-yes sudo python3
 
 echo "N" | repo --color=never init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
 repo sync
 
+chown -R build:build /opt
+
+sudo -s -H -u build MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment
+find . -name auto.conf
 echo "OSF_LMP_GIT_URL=\"github.com\"" >> build-ledge-gw/conf/auto.conf
 echo "OSF_LMP_GIT_NAMESPACE=\"OpenSourceFoundries/\"" >> build-ledge-gw/conf/auto.conf
 
-sudo -H -u build MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment
-sudo -H -u build bitbake lmp-gateway-image
+sudo -s -H -u build bash -c 'MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment && bitbake lmp-gateway-image'
