839a3f1629974f8e79fcfdfd7931efcce1fda730
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-07-12 18:17:28 +0300

    fix1

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -x
 
 set -e
 cd ~
@@ -8,7 +8,7 @@ cd oe-linaro-ledge
 git config --global user.email "ci@linaro.org"
 git config --global user.name "LEGE CI"
 
-repo --color=never init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
+echo "N" | repo --color=never init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
 repo sync
 
 MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment
