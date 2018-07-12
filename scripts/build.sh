a8427e747961ef4a92438af75a998f1da64fab3c
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-07-12 18:06:43 +0300

    repo: dont ask for color
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,7 +8,7 @@ cd oe-linaro-ledge
 git config --global user.email "ci@linaro.org"
 git config --global user.name "LEGE CI"
 
-repo init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
+repo --color=never init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
 repo sync
 
 MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment
