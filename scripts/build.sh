7722d9c49f782e8829fc95200610f14401f260af
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-07-12 18:44:05 +0300

    add osf lines
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -13,5 +13,8 @@ apt-get install sudo
 echo "N" | repo --color=never init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
 repo sync
 
+echo "OSF_LMP_GIT_URL=\"github.com\"" >> build-ledge-gw/conf/auto.conf
+echo "OSF_LMP_GIT_NAMESPACE=\"OpenSourceFoundries/\"" >> build-ledge-gw/conf/auto.conf
+
 sudo -H -u build MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment
 sudo -H -u build bitbake lmp-gateway-image
