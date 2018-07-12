198f4d43a4a6d11ca2d223e08a01511eacee3599
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-07-12 17:53:11 +0300

    add git user
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,6 +5,9 @@ cd ~
 mkdir oe-linaro-ledge
 cd oe-linaro-ledge
 
+git config --global user.email "ci@linaro.org"
+git config --global user.name "LEGE CI"
+
 repo init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
 repo sync
 
