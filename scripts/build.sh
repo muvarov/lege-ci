94bb0e94401b044a09654d13d1febb8cfe6c5381
Maxim Uvarov <maxim.uvarov@linaro.org>
2018-07-13 17:06:18 +0300

    add more deps
    Signed-off-by: Maxim Uvarov <maxim.uvarov@linaro.org>

--- scripts/build.sh
+++ scripts/build.sh
@@ -10,7 +10,7 @@ cd oe-linaro-ledge
 git config --global user.email "ci@linaro.org"
 git config --global user.name "LEGE CI"
 adduser --disabled-password --gecos "" build
-apt-get install -y --force-yes sudo python3
+apt-get install -y --force-yes sudo python3 python-pexpect python3-distutils language-pack-en-base locales chrpath cpio diffstat g++ gawk make wget texinfo
 
 echo "N" | repo --color=never init -u https://git.linaro.org/people/christophe.priouzeau/oe-lite-manifest.git
 repo sync
@@ -22,4 +22,4 @@ find . -name auto.conf
 echo "OSF_LMP_GIT_URL=\"github.com\"" >> build-ledge-gw/conf/auto.conf
 echo "OSF_LMP_GIT_NAMESPACE=\"OpenSourceFoundries/\"" >> build-ledge-gw/conf/auto.conf
 
-sudo -s -H -u build bash -c 'MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment && bitbake lmp-gateway-image'
+sudo -s -H -u build bash -c 'MACHINE=ledge-ti-am572x DISTRO=ledge-gw source ./setup-environment && export LANG=en_US.UTF-8 LC_ALL=en_US.UTF-8 LANGUAGE=en_US.UTF-8 && bitbake lmp-gateway-image'
